a457254629cb43864e939e01ba6098fce2f0135d
Andre-Gross <andre.handy94@gmail.com>
2025-11-24 19:49:10 +0100

    feat(firebase): add Firestore service for flashcards collection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ projectId: "flashcard-system-24f4b", appId: "1:722426904586:web:0a3379f355bfb859fb1ff3", storageBucket: "flashcard-system-24f4b.firebasestorage.app", apiKey: "AIzaSyAlglYjDwVSqziUwC0_tDluiAqOkqGXXWk", authDomain: "flashcard-system-24f4b.firebaseapp.com", messagingSenderId: "722426904586", projectNumber: "722426904586", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
