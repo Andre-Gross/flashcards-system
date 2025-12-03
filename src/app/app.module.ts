2f2bf15744cae81ae9b9e7b1d520e06bb30b9a01
Andre-Gross <andre.handy94@gmail.com>
2025-12-03 18:14:54 +0100

    refactor(app): remove auto-generated Firebase config lines to fix Firestore connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ projectId: "flashcard-system-24f4b", appId: "1:722426904586:web:0a3379f355bfb859fb1ff3", storageBucket: "flashcard-system-24f4b.firebasestorage.app", apiKey: "AIzaSyAlglYjDwVSqziUwC0_tDluiAqOkqGXXWk", authDomain: "flashcard-system-24f4b.firebaseapp.com", messagingSenderId: "722426904586", projectNumber: "722426904586", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ projectId: "flashcard-system-24f4b", appId: "1:722426904586:web:0a3379f355bfb859fb1ff3", storageBucket: "flashcard-system-24f4b.firebasestorage.app", apiKey: "AIzaSyAlglYjDwVSqziUwC0_tDluiAqOkqGXXWk", authDomain: "flashcard-system-24f4b.firebaseapp.com", messagingSenderId: "722426904586"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
