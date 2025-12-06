742c24f39768d28bcac1745069a37af466380c97
Andre-Gross <andre.handy94@gmail.com>
2025-12-06 18:00:37 +0100

    chore(home): add temporary test subscriptions for FlashcardsService

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
 import { FlashcardsService } from '../firebase-service/flashcards.service';
 
+import { Component, inject, DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+    standalone: false,
 })
 export class HomePage {
+    private flashcardsService = inject(FlashcardsService);
+    private destroyRef = inject(DestroyRef);
 
-    constructor(private flashcardsService: FlashcardsService) {
-        flashcardsService.getFlashcardCollections();
+
+    constructor() {}
+
+
+    ngOnInit() {
+        this.flashcardsService.getCollectionList('flashcard-collections')
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((collections) => {
+                console.log("List of flashcard collections: ", collections);
+            });
+
+        this.flashcardsService.getFlashcards("2Nd45CKgRGpBGhcLmKHv")
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((flashcards) => {
+                console.log("Flashcards: ", flashcards);
+            });
     }
 
 }
