2d4ab05a6180842abf2f724161b64a6ea3131526
Andre-Gross <andre.handy94@gmail.com>
2025-12-13 12:49:54 +0100

    refactor(flashcards): map Firestore deck data into domain model

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { FlashcardsService } from '../firebase-service/flashcards.service';
+import { FlashcardCollection } from '../interfaces/flashcard-collection.interface';
 
 @Component({
     selector: 'app-home',
@@ -6,8 +10,25 @@ import { Component } from '@angular/core';
     styleUrls: ['home.page.scss'],
     standalone: false,
 })
+
 export class HomePage {
+    private flashcardsService = inject(FlashcardsService);
+    decklist: FlashcardCollection[] = [];
 
-    constructor() { }
-
+    constructor() {
+        this.flashcardsService.getCollectionList().subscribe(data => {
+            this.decklist = [];
+            data.forEach((element: any) => {
+                this.decklist.push({
+                    deckId: element.id,
+                    ownerId: element.ownerId,
+                    editorIds: element.editorIds,
+                    subscriberIds: element.subscriberIds,
+                    title: element.title,
+                    description: element.description,
+                    flashcards: element.flashcards,
+                });
+            });
+        });
+    }
 }
