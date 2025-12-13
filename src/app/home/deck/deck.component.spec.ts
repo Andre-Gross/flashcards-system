699506febf2c0ed10386e30309f991a971b67f5d
Andre-Gross <andre.handy94@gmail.com>
2025-12-13 12:43:05 +0100

    refactor(home): rename CollectionComponent to DeckComponent and move it into home folder

--- src/app/home/deck/deck.component.spec.ts
+++ src/app/home/deck/deck.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
-import { CollectionComponent } from './collection.component';
+import { DeckComponent } from './deck.component';
 
-describe('CollectionComponent', () => {
-  let component: CollectionComponent;
-  let fixture: ComponentFixture<CollectionComponent>;
+describe('DeckComponent', () => {
+  let component: DeckComponent;
+  let fixture: ComponentFixture<DeckComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [CollectionComponent],
+      imports: [DeckComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(CollectionComponent);
+    fixture = TestBed.createComponent(DeckComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
--- src/app/home/deck/deck.component.ts
+++ src/app/home/deck/deck.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-collection',
-  templateUrl: './collection.component.html',
-  styleUrls: ['./collection.component.scss'],
+  selector: 'app-deck',
+  templateUrl: './deck.component.html',
+  styleUrls: ['./deck.component.scss'],
   standalone: true,
 })
 export class CollectionComponent  implements OnInit {
