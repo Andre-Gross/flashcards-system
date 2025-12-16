148ee1070a9120d4213ba662d58dc3d0e6674dd4
SamuelSchnuell <32879617+Blackjack1000@users.noreply.github.com>
2025-12-16 11:09:35 +0100

    feat(deck): detect long-press gesture

--- src/app/home/deck/deck.component.ts
+++ src/app/home/deck/deck.component.ts
@@ -1,16 +1,57 @@
-import { Component, Input, OnInit } from '@angular/core';
+// import { Component, Input, OnInit } from '@angular/core';
+// import { IonCard, IonCardHeader, IonCardContent } from "@ionic/angular/standalone";
+import { Component, ElementRef, ViewChild, Input } from '@angular/core';
+import { GestureController, IonCard, IonCardContent, IonCardHeader } from '@ionic/angular/standalone';
+
 
 @Component({
     selector: 'app-deck',
     templateUrl: './deck.component.html',
     styleUrls: ['./deck.component.scss'],
     standalone: true,
+    imports: [IonCardContent, IonCardHeader, IonCard],
 })
-export class DeckComponent implements OnInit {
+export class DeckComponent {
     @Input() deck: any;
 
-    constructor() { }
+    @ViewChild('card', { read: ElementRef }) card!: ElementRef<HTMLIonCardElement>;
 
-    ngOnInit() { } 
+    private longPressTimer: any;
+    private readonly longPressMinDuration: number = 400; 
 
+    constructor(private el: ElementRef, private gestureCtrl: GestureController) {}
+
+    ngAfterViewInit() {
+        const gesture = this.gestureCtrl.create({
+        el: this.card.nativeElement,
+        gestureName: 'long-press',
+        threshold: 0, 
+        
+        onStart: () => this.onPressStart(), 
+        
+        onEnd: () => this.onPressEnd(), 
+        
+        onMove: () => this.onPressMove(),
+        });
+
+        gesture.enable();
+   }
+
+    private onPressStart() {
+        this.longPressTimer = setTimeout(() => {
+        this.onLongPressDetected();
+        }, this.longPressMinDuration);
+    }
+
+    private onPressEnd() {
+        clearTimeout(this.longPressTimer);
+    }
+
+    private onPressMove() {
+        clearTimeout(this.longPressTimer);
+    }
+
+    private onLongPressDetected() {
+        console.log('--- LONG PRESS DETECTED ---');
+    }
 }
